08a337a9a436f59e549b2665b3a2431a9484b265
Stanislau <savandex@gmail.com>
2020-10-16 22:37:03 -0700

    Добавляет сборщик webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './js/utils.js',
+    './js/debounce.js',
+    './js/backend.js',
+    './js/render.js',
+    './js/data.js',
+    './js/stat.js',
+    './js/modal.js',
+    './js/move.js',
+    './js/wizard.js',
+    './js/setup.js',
+    './js/game.js'
+  ],
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname),
+    iife: true
+  },
+
+  devtool: false
+
+};
